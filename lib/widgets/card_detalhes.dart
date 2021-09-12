63ea7ff19afe24db6bccc18a4cbac3cac3904e58
Edilson Jardim Filho <edilsonj.filho@gmail.com>
2021-09-12 14:00:00 -0300

    Configuraçao de internacionalizacao e cor

--- lib/widgets/card_detalhes.dart
+++ lib/widgets/card_detalhes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '/model/movel.dart';
 import '/widgets/texto_detalhes.dart';
@@ -7,6 +8,11 @@ class CardDetalhes extends StatelessWidget {
   final Movel movel;
   const CardDetalhes({Key? key, required this.movel}) : super(key: key);
 
+  formatacaoReais(int preco) {
+    final currencyReais = NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$');
+    return currencyReais.format(preco);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -14,6 +20,19 @@ class CardDetalhes extends StatelessWidget {
         children: [
           TextoDetalhes(texto: movel.titulo),
           TextoDetalhes(texto: movel.descricao),
+          Row(
+            children: [
+              Text(formatacaoReais(movel.preco)),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  primary: Colors.black,
+                ),
+                onPressed: () {},
+                child: Text('Comprar'),
+              )
+            ],
+          )
         ],
       ),
     );
