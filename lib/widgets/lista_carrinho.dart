31c38cbc544f87feb91e2d87fbdcab2b22606654
Edilson Jardim Filho <edilsonj.filho@gmail.com>
2021-09-17 21:34:19 -0300

    02 Alinhando Informaçoes
    Ajuste no Card do listView.
    
    Titulo, valor e qtdade sem formataçao.
    
    Alinhado os itens no card e adicionado gestureDetector
    
    by: Edilson Jardim Filhlo <edilsonj.filho@gmail.com>

--- lib/widgets/lista_carrinho.dart
+++ lib/widgets/lista_carrinho.dart
@@ -7,6 +7,14 @@ class ListaCarrinho extends StatelessWidget {
   final List<ItemCarrinho> carrinho = Inicio.itensCarrinho;
   ListaCarrinho({Key? key}) : super(key: key);
 
+  _aumentarQuantidade(ItemCarrinho item) {
+    item.quantidade++;
+  }
+
+  _dimnuiQuantidade(ItemCarrinho item) {
+    item.quantidade--;
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -24,15 +32,48 @@ class ListaCarrinho extends StatelessWidget {
                     'lib/assets/imagens/${movel.foto}',
                     height: 92,
                   ),
-                  Container(
-                    margin: EdgeInsets.only(left: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(movel.titulo),
-                        Text('${movel.preco}'),
-                        Text('${item.quantidade}'),
-                      ],
+                  Expanded(
+                    child: Container(
+                      height: 92,
+                      margin: EdgeInsets.only(left: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(movel.titulo),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('${movel.preco}'),
+                              Row(
+                                children: [
+                                  GestureDetector(
+                                    onTap: () => _aumentarQuantidade(item),
+                                    child: Container(
+                                      margin: EdgeInsets.all(8),
+                                      child: Icon(
+                                        Icons.add,
+                                        size: 14,
+                                      ),
+                                    ),
+                                  ),
+                                  Text('${item.quantidade}'),
+                                  GestureDetector(
+                                    onTap: () => _dimnuiQuantidade(item),
+                                    child: Container(
+                                      margin: EdgeInsets.all(8),
+                                      child: Icon(
+                                        Icons.remove,
+                                        size: 14,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
